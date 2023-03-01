85be0df1889b69d668233baf3b21ac0c612d07a5
chaffexd <shanechaffe@gmail.com>
2023-03-01 16:43:18 +0000

    refactor input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useReducer, useContext } from 'react';
 
+import Input from '../UI/Input/Input';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
@@ -101,20 +102,14 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input 
+          id="email" 
+          label="E-Mail" 
+          type="email" 
+          isValid={emailIsValid} 
+          value={emailState.value} 
+          onChange={emailChangeHandler} 
+          onBlur={validateEmailHandler} />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ''
